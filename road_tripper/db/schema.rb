aecf2c25be4981f4999ea2dc369e8539e78dd15d
barrygrubb <barrygrubb@users.noreply.github.com>
2016-03-24 21:57:49 +0000

    Created and passed shoulda tests for associations in Profile and User models

--- road_tripper/db/schema.rb
+++ road_tripper/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324214749) do
+ActiveRecord::Schema.define(version: 20160324215516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,10 @@ ActiveRecord::Schema.define(version: 20160324214749) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "road_trips", force: :cascade do |t|
     t.string   "title"
@@ -62,4 +65,5 @@ ActiveRecord::Schema.define(version: 20160324214749) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "itineraries", "road_trips"
+  add_foreign_key "profiles", "users"
 end
