c5df717b9da74570918a4cad409c63c6b12a0046
Frazer Watson <frazerw@tutanota.com>
2016-03-24 16:12:26 +0000

    Unit/model test passed - Creating a road trip completed.

--- road_tripper/spec/models/road_trip_spec.rb
+++ road_tripper/spec/models/road_trip_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe RoadTrip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it 'created with a title' do
+    road_trip = RoadTrip.new(title: "Going Home")
+    expect(road_trip.title).to eq 'Going Home'
+  end
 end
