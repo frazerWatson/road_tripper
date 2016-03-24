a91f7d67fb35984fa928f714fb3d5cd584ae1c2f
Frazer Watson <frazerw@tutanota.com>
2016-03-24 15:49:30 +0000

    Creating a road trip feature test passing.

--- road_tripper/spec/features/road_trips_feature_spec.rb
+++ road_tripper/spec/features/road_trips_feature_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 feature 'road_trip' do
+
   context 'no road trips have been added' do
     scenario 'should display a prompt to add a road trip' do
       visit '/'
@@ -8,4 +9,16 @@ feature 'road_trip' do
       expect(page).to have_link 'New Road trip'
     end
   end
+
+  context 'adding a road trip' do
+     before do
+       RoadTrip.create(title: 'Long journey')
+     end
+    scenario 'should display a new road trip' do
+      visit '/'
+      expect(page).to have_content 'Long journey'
+      expect(page).to_not have_content 'No road trips yet'
+    end
+  end
+
 end
