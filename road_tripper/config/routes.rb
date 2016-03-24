709a3170afe6adecadc1ae5938f7c3e59877459b
barrygrubb <barrygrubb@users.noreply.github.com>
2016-03-24 21:48:45 +0000

    Added Devise Users and also Profiles

--- road_tripper/config/routes.rb
+++ road_tripper/config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  resources :profiles
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   resources :itineraries
   resources :road_trips
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root 'road_trips#index'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
