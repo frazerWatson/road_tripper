ed4781026bb170656e3fa54c163de61528387344
barrygrubb <barrygrubb@users.noreply.github.com>
2016-03-24 17:57:38 +0000

    Added feature tests for basic CRUD functions

--- road_tripper/spec/features/road_trips_feature_spec.rb
+++ road_tripper/spec/features/road_trips_feature_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
+require 'capybara/dsl'
+require 'selenium-webdriver'
 
 feature 'road_trip' do
+
+  before do
+     include Capybara::DSL
+     Capybara.default_driver = :selenium
+  end
 
   context 'no road trips have been added' do
     scenario 'should display a prompt to add a road trip' do
@@ -11,14 +18,40 @@ feature 'road_trip' do
   end
 
   context 'adding a road trip' do
-     before do
-       RoadTrip.create(title: 'Long journey')
-     end
     scenario 'should display a new road trip' do
       visit '/'
+      click_link('New Road trip')
+      fill_in 'Title', with: 'Long journey'
+      click_button('Create Road trip')
       expect(page).to have_content 'Long journey'
       expect(page).to_not have_content 'No road trips yet'
     end
   end
 
+  context 'edit a road trip' do
+    before do
+      road_trip = RoadTrip.create(title: 'Long journey')
+    end
+    scenario 'should display the updated road trip details' do
+      visit '/'
+      click_link('Edit')
+      fill_in 'Title', with: 'A short journey'
+      click_button('Update Road trip')
+      expect(page).to have_content 'A short journey'
+      expect(page).to_not have_content 'Long journey'
+    end
+  end
+
+  context 'delete a road trip' do
+    before do
+      road_trip = RoadTrip.create(title: 'Long journey')
+    end
+    scenario 'should display the updated road trip details' do
+      visit '/'
+      page.accept_confirm do
+        click_link ('Destroy')
+      end
+      expect(page).to_not have_content 'Long journey'
+    end
+  end
 end
