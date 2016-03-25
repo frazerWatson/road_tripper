8a94287f4f1a8a5473c9fb89567bb0825dba8982
Godfrey Chiu <ggwc82@gmail.com>
2016-03-25 16:57:09 +0000

    Passed itineraries feature test, displays itinerary details on the dashboard

--- road_tripper/spec/features/itineraries_feature_spec.rb
+++ road_tripper/spec/features/itineraries_feature_spec.rb
@@ -3,21 +3,32 @@ require 'capybara/dsl'
 require 'selenium-webdriver'
 
 feature 'road_trip' do
-
-  before do
-    road_trip = RoadTrip.create(title: 'Long journey')
-  end
+before do
+    include Capybara::DSL
+    Capybara.default_driver = :selenium
+end
 
   scenario 'allows user to add itinerary' do
     visit '/'
-    click_link 'Show'
+    click_link 'Sign up'
+    fill_in 'Email', with: 'a@a.com'
+    fill_in 'Password', with: '88888888'
+    fill_in 'Password confirmation', with: '88888888'
+    fill_in 'Username', with: 'DanH'
+    fill_in 'Hometown', with: 'London'
+    fill_in 'First name', with: 'Dan'
+    fill_in 'Last name', with: 'Hash'
+    click_button 'Sign up'
+    click_link 'New Road trip'
+    fill_in 'Title', with: 'New Long Journey to Space'
+    click_button 'Create Road trip'
     click_link 'Dashboard'
     click_link 'Add Itinerary'
     fill_in 'Origin', with: 'London'
     fill_in 'Destination', with: 'Fiji/Essex'
     select '2016', from: 'itinerary[start_date(1i)]'
     click_button 'Create Itinerary'
-    expect(current_path).to eq 'road_trips/1/edit'
+    expect(current_path).to eq "/road_trips/1/edit"
     expect(page).to have_content 'London'
     expect(page).to_not have_link 'Add Itinerary'
   end
--- road_tripper/spec/spec_helper.rb
+++ road_tripper/spec/spec_helper.rb
@@ -11,6 +11,7 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
+require 'support/database_cleaner'
 #
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
