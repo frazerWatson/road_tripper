71ed2978bb04afe04ac05e1426e7e6a18c82cf0e
Godfrey Chiu <ggwc82@gmail.com>
2016-03-26 16:50:15 +0000

    Implemented feature to add photos to posts and display photos on road trip profile

--- road_tripper/app/controllers/posts_controller.rb
+++ road_tripper/app/controllers/posts_controller.rb
@@ -70,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body, :road_trip_id)
+      params.require(:post).permit(:title, :body, :road_trip_id, :image)
     end
 end
--- road_tripper/db/schema.rb
+++ road_tripper/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326142602) do
+ActiveRecord::Schema.define(version: 20160326161855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,9 +42,13 @@ ActiveRecord::Schema.define(version: 20160326142602) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "road_trip_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "posts", ["road_trip_id"], name: "index_posts_on_road_trip_id", using: :btree
