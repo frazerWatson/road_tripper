1d2ab8b84620db6f56de16433651bd2a340f9087
Godfrey Chiu <ggwc82@gmail.com>
2016-03-30 14:01:17 +0100

    Added more post feature tests

--- app/models/road_trip.rb
+++ app/models/road_trip.rb
@@ -5,5 +5,4 @@ class RoadTrip < ActiveRecord::Base
 
   has_attached_file :feature_image, :styles => { :medium => "300x300>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :feature_image, :content_type => /\Aimage\/.*\Z/
-
 end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -27,7 +27,6 @@ feature 'Posting on road trips' do
   scenario 'user can delete a post' do
     create_new_post("Just about to set off")
     click_link 'View trip'
-    expect(page).to have_content "Just about to set off"
     click_link 'x'
     expect(page).to_not have_content "Just about to set off"    
   end
@@ -35,12 +34,30 @@ feature 'Posting on road trips' do
   scenario 'user can modify a post' do
     create_new_post("Just about to set off")
     click_link 'View trip'
-    expect(page).to have_content "Just about to set off"
     click_link 'Edit'
     fill_in 'Title', with: "I'm not going now"
     click_button 'Update Post'   
-    expect(page).to_not have_content "Just about to set off"  
-    expect(current_path).to eq "/road_trips/1"  
     expect(page).to have_content "I'm not going now"
+  end
+
+  scenario 'errors if creating a post without a title' do
+    create_new_post("")
+    expect(page).to have_content "Title can't be blank"
+  end
+
+  scenario 'errors if creating a post without a body' do
+    click_link 'Add new post'
+    fill_in 'Title', with: title
+    click_button 'Create Post'
+    expect(page).to have_content "Body can't be blank"
+  end
+
+  scenario 'errors if updating a post without a title' do
+    create_new_post("Just about to set off")
+    click_link 'View trip'
+    click_link 'Edit'
+    fill_in 'Title', with: ""
+    click_button 'Update Post'   
+    expect(page).to have_content "Title can't be blank"
   end
 end
