6700423459fdbaecb48048a59c6ff4fb6d2db919
Godfrey Chiu <ggwc82@gmail.com>
2016-03-31 21:35:53 +0100

    Implemented minimum characters on description of road trip

--- app/models/road_trip.rb
+++ app/models/road_trip.rb
@@ -5,6 +5,7 @@ class RoadTrip < ActiveRecord::Base
 
   validates :title, presence: true
   validates :description, presence: true
+  validates_length_of :description, :minimum => 80
 
   has_attached_file :feature_image, :styles => { :medium => "300x300>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :feature_image, :content_type => /\Aimage\/.*\Z/
